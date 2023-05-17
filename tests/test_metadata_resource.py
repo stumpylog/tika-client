cbdd5c251d3c642c27eca9346d9162ac8555f93b
Trenton H <797416+stumpylog@users.noreply.github.com>
2023-05-17 11:44:15 -0700

    Moving more towards pytest

--- tests/test_metadata_resource.py
+++ tests/test_metadata_resource.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from datetime import timezone
 
 import magic
+import pytest
 from pytest_httpx import HTTPXMock
 
 from tests.config import SAMPLE_DIR
@@ -15,34 +16,35 @@ class TestMetadataResource:
         test_file = SAMPLE_DIR / "sample.docx"
         resp = tika_client.metadata.from_file(test_file, magic.from_file(str(test_file), mime=True))
 
-        self.assertEqual(resp.type, "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
-        self.assertIsNone(resp.created)
+        assert resp.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        assert resp.created is None
 
     def test_metadata_from_docx_no_mime(self, tika_client):
         test_file = SAMPLE_DIR / "sample.docx"
         resp = tika_client.metadata.from_file(test_file)
 
-        self.assertEqual(resp.type, "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
-        self.assertIsNone(resp.created)
+        assert resp.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        assert resp.created is None
 
     def test_metadata_from_word_docx(self, tika_client):
         test_file = SAMPLE_DIR / "microsoft-sample.docx"
         resp = tika_client.metadata.from_file(test_file, magic.from_file(str(test_file), mime=True))
 
-        self.assertEqual(resp.type, "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
-        self.assertEqual(resp.created, datetime(year=2023, month=5, day=17, hour=16, minute=41, tzinfo=timezone.utc))
-        self.assertEqual(resp.modified, datetime(year=2023, month=5, day=17, hour=16, minute=44, tzinfo=timezone.utc))
+        assert resp.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+        assert resp.created == datetime(year=2023, month=5, day=17, hour=16, minute=41, tzinfo=timezone.utc)
+        assert resp.modified == datetime(year=2023, month=5, day=17, hour=16, minute=44, tzinfo=timezone.utc)
 
     def test_metadata_from_odt(self, tika_client):
         test_file = SAMPLE_DIR / "sample.odt"
         resp = tika_client.metadata.from_file(test_file, magic.from_file(str(test_file), mime=True))
 
-        self.assertEqual(resp.type, "application/vnd.oasis.opendocument.text")
-        self.assertEqual(resp.data["generator"], "LibreOfficeDev/6.0.5.2$Linux_X86_64 LibreOffice_project/")
-        self.assertIsNone(resp.created)
+        assert resp.type == "application/vnd.oasis.opendocument.text"
+        assert resp.data["generator"] == "LibreOfficeDev/6.0.5.2$Linux_X86_64 LibreOffice_project/"
+        assert resp.created is None
 
     def test_http_error(self, httpx_mock: HTTPXMock):
+        test_file = SAMPLE_DIR / "sample.odt"
+
         httpx_mock.add_response(status_code=500)
-        with self.assertRaises(RestHttpError), TikaClient(base_url=TIKA_URL) as client:
-            test_file = SAMPLE_DIR / "sample.odt"
+        with pytest.raises(RestHttpError), TikaClient(base_url=TIKA_URL) as client:
             client.metadata.from_file(test_file)
