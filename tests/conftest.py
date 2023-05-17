1ed3986155b4f94780dd13d537aa246c1071b4bd
Trenton H <797416+stumpylog@users.noreply.github.com>
2023-05-17 11:05:06 -0700

    Use pytest-httpx to handle better mocking

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+from tika_rest_client.client import TikaClient
+
+from .config import TIKA_URL
+
+
+@pytest.fixture
+def tika_client() -> TikaClient:
+    with TikaClient(base_url=TIKA_URL) as client:
+        yield client
--- tests/test_metadata_resource.py
+++ tests/test_metadata_resource.py
@@ -1,15 +1,8 @@
-import contextlib
 from datetime import datetime
 from datetime import timezone
-from unittest import TestCase
 
 import magic
-
-with contextlib.suppress(ImportError):
-    pass
-
-
-from unittest import mock
+from pytest_httpx import HTTPXMock
 
 from tests.config import SAMPLE_DIR
 from tests.config import TIKA_URL
@@ -17,49 +10,39 @@ from tika_rest_client.client import TikaClient
 from tika_rest_client.errors import RestHttpError
 
 
-class TestMetadataResource(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.client = TikaClient(base_url=TIKA_URL)
-
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.client.close()
-        cls.client = None
-
-    def test_metadata_from_docx(self):
+class TestMetadataResource:
+    def test_metadata_from_docx(self, tika_client):
         test_file = SAMPLE_DIR / "sample.docx"
-        resp = self.client.metadata.from_file(test_file, magic.from_file(str(test_file), mime=True))
+        resp = tika_client.metadata.from_file(test_file, magic.from_file(str(test_file), mime=True))
 
         self.assertEqual(resp.type, "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
         self.assertIsNone(resp.created)
 
-    def test_metadata_from_docx_no_mime(self):
+    def test_metadata_from_docx_no_mime(self, tika_client):
         test_file = SAMPLE_DIR / "sample.docx"
-        resp = self.client.metadata.from_file(test_file)
+        resp = tika_client.metadata.from_file(test_file)
 
         self.assertEqual(resp.type, "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
         self.assertIsNone(resp.created)
 
-    def test_metadata_from_word_docx(self):
+    def test_metadata_from_word_docx(self, tika_client):
         test_file = SAMPLE_DIR / "microsoft-sample.docx"
-        resp = self.client.metadata.from_file(test_file, magic.from_file(str(test_file), mime=True))
+        resp = tika_client.metadata.from_file(test_file, magic.from_file(str(test_file), mime=True))
 
         self.assertEqual(resp.type, "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
         self.assertEqual(resp.created, datetime(year=2023, month=5, day=17, hour=16, minute=41, tzinfo=timezone.utc))
         self.assertEqual(resp.modified, datetime(year=2023, month=5, day=17, hour=16, minute=44, tzinfo=timezone.utc))
 
-    def test_metadata_from_odt(self):
+    def test_metadata_from_odt(self, tika_client):
         test_file = SAMPLE_DIR / "sample.odt"
-        resp = self.client.metadata.from_file(test_file, magic.from_file(str(test_file), mime=True))
+        resp = tika_client.metadata.from_file(test_file, magic.from_file(str(test_file), mime=True))
 
         self.assertEqual(resp.type, "application/vnd.oasis.opendocument.text")
         self.assertEqual(resp.data["generator"], "LibreOfficeDev/6.0.5.2$Linux_X86_64 LibreOffice_project/")
         self.assertIsNone(resp.created)
 
-    def test_http_error(self):
-        with mock.patch("tika_rest_client.client.TikaClient.put") as mocked:
-            mocked.return_value.status_code = 500
-            with self.assertRaises(RestHttpError):
-                test_file = SAMPLE_DIR / "sample.odt"
-                self.client.metadata.from_file(test_file)
+    def test_http_error(self, httpx_mock: HTTPXMock):
+        httpx_mock.add_response(status_code=500)
+        with self.assertRaises(RestHttpError), TikaClient(base_url=TIKA_URL) as client:
+            test_file = SAMPLE_DIR / "sample.odt"
+            client.metadata.from_file(test_file)
