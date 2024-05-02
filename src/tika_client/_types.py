d86284136ed6b3fc56f9e4f9f560e2843ba07ccf
Trenton H <797416+stumpylog@users.noreply.github.com>
2024-05-02 11:39:39 -0700

    Feature: Use more default `hatch` commands for development (#17)

--- src/tika_client/_types.py
+++ src/tika_client/_types.py
@@ -1,5 +1,13 @@
+from __future__ import annotations
+
+import sys
 from typing import Optional
 from typing import Union
+
+if sys.version_info >= (3, 11):  # pragma: no cover
+    from typing import Self
+else:  # pragma: no cover
+    from typing_extensions import Self  # noqa: F401
 
 MimeType = Optional[str]
 
